c0ba2842491534d80c8233497f1cf42190e0900c
dbhausen <davidabettenhausen@msn.com>
2022-02-11 09:42:25 -0500

    new message

--- A_SITE/snippets/models.py
+++ A_SITE/snippets/models.py
@@ -12,7 +12,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-    """doc  hh"""
+    """doc  hhff"""
 
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default="")
