2da7c97e7172d3235f92ea827fc51935c796542a
dbhausen <davidabettenhausen@msn.com>
2022-02-11 07:49:26 -0500

    commit 2

--- A_SITE/annuity_app/models.py
+++ A_SITE/annuity_app/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+"""commit 2 change"""
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
