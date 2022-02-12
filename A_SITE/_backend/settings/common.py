6aba0be9a454611760ace471d78d155392fa2c08
dbhausen <davidabettenhausen@msn.com>
2022-02-12 11:06:40 -0500

    pagintion

--- A_SITE/_backend/settings/common.py
+++ A_SITE/_backend/settings/common.py
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 10,
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
