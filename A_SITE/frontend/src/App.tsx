c92df78b93b94ebb2e61e4172d9d23388a7a31aa
dbhausen <davidabettenhausen@msn.com>
2022-02-13 15:19:58 -0500

    test b

--- A_SITE/frontend/src/App.tsx
+++ A_SITE/frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload fff.
+          Edit <code>src/App.tsx</code> and save to reload need build.
         </p>
         <a
           className="App-link"
