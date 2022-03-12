dd30001e9a319d363016bb2d0b46f558c47e55b3
dbhausen <davidabettenhausen@msn.com>
2022-03-12 08:08:22 -0500

    url

--- A_SITE/_backend/urls.py
+++ A_SITE/_backend/urls.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html")),
     path("polls/", include("annuity_app.urls")),
     path("rest/", include("quickstart.urls")),
     path("snip/", include("snippets.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("/", TemplateView.as_view(template_name="index.html")),
 ]
