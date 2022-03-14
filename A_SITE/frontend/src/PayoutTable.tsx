3671838b647fa4dbfda068f39edc1c64b304158a
dbhausen <davidabettenhausen@msn.com>
2022-03-14 09:32:56 -0400

    table width

--- A_SITE/frontend/src/PayoutTable.tsx
+++ A_SITE/frontend/src/PayoutTable.tsx
@@ -132,7 +132,7 @@ export default function StickyHeadTable() {
 	)
 
 	return (
-		<Paper sx={{ width: 300, overflow: 'hidden' }}>
+		<Paper sx={{ width: '100%', overflow: 'hidden' }}>
 			<TableContainer sx={{ maxHeight: 440 }}>
 				<Table stickyHeader aria-label='sticky table'>
 					<TableHead>
