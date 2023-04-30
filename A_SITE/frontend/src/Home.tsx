47b7a96223e5f1ed2756a7efb3b0e96967d93fae
dbhausen <davidabettenhausen@msn.com>
2023-04-30 12:35:00 -0400

    no exclimation

--- A_SITE/frontend/src/Home.tsx
+++ A_SITE/frontend/src/Home.tsx
@@ -40,7 +40,7 @@ const Home = () => {
 				sx={{ textAlign: 'center', padding: '15px' }}
 				gutterBottom
 			>
-				The retirement planning tools you always wanted!!!!!2.
+				The retirement planning tools you always wanted.
 			</Typography>
 			<Typography
 				component='div'
