6b36142132cb93ba266a355f9eb46a5293d11e4d
dbhausen <davidabettenhausen@msn.com>
2023-05-14 10:49:41 -0400

    login 2

--- A_SITE/frontend/src/Env.tsx
+++ A_SITE/frontend/src/Env.tsx
@@ -1,9 +1,9 @@
 const env = () => {
-	const d = { API_HOST: 'https://dbhausen.pythonanywhere.com' }
+	const d = { API_HOST: 'https://www.restreact.com' }
 	if (process.env.NODE_ENV === 'development') {
 		d.API_HOST = 'http://localhost:8000'
 	} else if (process.env.NODE_ENV === 'production') {
-		d.API_HOST = 'https://dbhausen.pythonanywhere.com'
+		d.API_HOST = 'https://www.restreact.com'
 	}
 	return d
 }
